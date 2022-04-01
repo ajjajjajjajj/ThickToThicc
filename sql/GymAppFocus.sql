28fd195817f42f8e779571fdd8a25fe6f1350a68
lyqzeo <101164063+lyqzeo@users.noreply.github.com>
2022-04-01 12:40:44 +0800

    Create GymAppFocus.sql

---
+++ sql/GymAppFocus.sql
@@ -0,0 +1,12 @@
+INSERT INTO focus VALUES
+    ('Pilates'),
+    ('Yoga'),
+    ('Weightlifting'),
+    ('Spin'),
+    ('Crossfit'),
+    ('Calisthenics'),
+    ('Powerlifting'),
+    ('Martial Arts'),
+    ('Physiotherapy'),
+    ('Running'),
+	  ('Swimming');
