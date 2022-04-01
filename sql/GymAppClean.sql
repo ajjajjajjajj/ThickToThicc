9bb99c6e6c774ce01c45c826bb911cf65e729eb6
lyqzeo <101164063+lyqzeo@users.noreply.github.com>
2022-04-01 13:44:16 +0800

    Update GymAppClean.sql

--- sql/GymAppClean.sql
+++ sql/GymAppClean.sql
@@ -3,7 +3,16 @@
   Cleaning script
 
 *******************/
-
+DROP TRIGGER IF EXISTS calc_gym on member_gym;
+DROP TRIGGER IF EXISTS calc_trainer on member_trainer;
+DROP TRIGGER IF EXISTS insert_trainer on trainer;
+DROP TRIGGER IF EXSITS insert_gym on gym;
+DROP FUNCTION IF EXISTS gym_ratings();
+DROP FUNCTION IF EXISTS train_ratings();
+DROP FUNCTION IF EXISTS insert_gym();
+DROP FUNCTION IF EXISTS insert_trainer();
+DROP TABLE IF EXISTS gym_ratings;
+DROP TABLE IF EXISTS trainer_ratings;
 DROP TABLE IF EXISTS member_gym;
 DROP TABLE IF EXISTS member_trainer;
 DROP TABLE IF EXISTS gymfocus;
@@ -12,3 +21,4 @@ DROP TABLE IF EXISTS member;
 DROP TABLE IF EXISTS trainer;
 DROP TABLE IF EXISTS login;
 DROP TABLE IF EXISTS focus;
+
