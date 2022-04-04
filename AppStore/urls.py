770af4554f3aaf2cf728b154abfd5a579f5084f2
Zoe Lua <e056431@u.nus.edu>
2022-04-04 17:19:59 +0800

    we

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('search', search_view),
     path('search_request', search_request, name='search_request'),
     path('login/',login_request,name='login'),
-    path('login/<str:type>/', logged_home, name = 'loggedhome'),
+    path('<str:fname>', logged_home, name = 'loggedhome'),
 ]
