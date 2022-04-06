c739222624e458314b57f597e0c363ff1515f904
Zoe Lua <e056431@u.nus.edu>
2022-04-06 15:34:43 +0800

    ewew

--- app/views.py
+++ app/views.py
@@ -240,7 +240,7 @@ def login_request(request):
     return render(request, "registration/login.html", context)
 
 
-def logged_home(request, type, id):
+def logged_home(request, *):
     return HttpResponse("hi")
 
 
