6e3eb731f81aa228f9f44ae8c617586df274fa04
Jing Jie <angjingjie@u.nus.edu>
2022-04-08 15:44:29 +0800

    ..

--- app/views.py
+++ app/views.py
@@ -63,10 +63,11 @@ def admin_edit_action(request):
             return render(request, 'app/index.html', {'users': users, 'status': 'Gym details edited successfully'})
         
         elif type == 'member':
-            cursor.execute("UPDATE member SET first_name = %s, last_name = %s, gender = %s, preferred_gym_location = %s, \
-                budget = %s, focus1 = %s, focus2 = %s, focus3 = %s WHERE email = %s",[request.POST['first_name'],request.POST['last_name'],
-                request.POST['gender'], request.POST['level'], request.POST['location'], request.POST['budget'],
-                request.POST['focus1'],request.POST['focus2'],request.POST['focus3'],request.POST['email']])
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE member SET first_name = %s, last_name = %s, gender = %s, preferred_gym_location = %s, \
+                    budget = %s, focus1 = %s, focus2 = %s, focus3 = %s WHERE email = %s",[request.POST['first_name'],request.POST['last_name'],
+                    request.POST['gender'], request.POST['level'], request.POST['location'], request.POST['budget'],
+                    request.POST['focus1'],request.POST['focus2'],request.POST['focus3'],request.POST['email']])
 
             with connection.cursor() as cursor:
                 cursor.execute("SELECT * FROM login")
@@ -74,11 +75,12 @@ def admin_edit_action(request):
             return render(request, 'app/index.html', {'users': users, 'status': 'Member details edited successfully'})
             
         elif type == 'trainer':
-            cursor.execute("UPDATE trainer SET first_name = %s, last_name = %s, gender = %s, upper_price_range = %s, \
-                    lower_price_range = %s, experience = %s, focus1 = %s, focus2 = %s, focus3 = %s, level = %s WHERE email = %s"
-                    , [request.POST['first_name'], request.POST['last_name'], request.POST['gender'],
-                        request.POST['upper_price_range'] , request.POST['lower_price_range'], request.POST['experience'], 
-                        request.POST['focus1'],request.POST['focus2'],request.POST['focus3'],request.POST['level'],request.POST['email']])
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE trainer SET first_name = %s, last_name = %s, gender = %s, upper_price_range = %s, \
+                        lower_price_range = %s, experience = %s, focus1 = %s, focus2 = %s, focus3 = %s, level = %s WHERE email = %s"
+                        , [request.POST['first_name'], request.POST['last_name'], request.POST['gender'],
+                            request.POST['upper_price_range'] , request.POST['lower_price_range'], request.POST['experience'], 
+                            request.POST['focus1'],request.POST['focus2'],request.POST['focus3'],request.POST['level'],request.POST['email']])
 
             with connection.cursor() as cursor:
                 cursor.execute("SELECT * FROM login")
